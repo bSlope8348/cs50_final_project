8d52fa941491f5e93097c00add0de51d891d5915
bSlope8348 <bSlope8348@gmail.com>
2025-09-11 11:54:51 -0600

    Chapter 10 tutorial complete

--- game/main.lua
+++ game/main.lua
@@ -1,7 +1,14 @@
 require("src.example")
-
+--require("lib.tick")
+local tick = require "lib.tick"
+local x = 30
+local y = 50
 function love.load()
+	
+	drawRectangle = false
+	tick.delay(function () drawRectangle = true end , 2)
 	ListOfRectangles = {}
+
 end
 
 function createRect()
@@ -18,16 +25,28 @@ function love.keypressed(key)
 	if key == "space" then
 		createRect()
 	end
+	    --If space is pressed then..
+    if key == "space" then
+        --x and y become a random number between 100 and 500
+        x = math.random(100, 500)
+        y = math.random(100, 500)
+    end
 end
 
 function love.update(dt)
+	tick.update(dt)
 	for i,rec in ipairs(ListOfRectangles) do
 		rec.x = rec.x + rec.speed * dt
 	end
 end
 
 function love.draw()
+	love.graphics.rectangle("line", x, y, 100, 100)
+	if drawRectangle then
+    	love.graphics.rectangle("fill", 100, 100, 300, 200)
+    end
 	for i,rec in ipairs(ListOfRectangles) do
 		love.graphics.rectangle("line", rec.x, rec.y, rec.width, rec.height)
 	end
 end
+
