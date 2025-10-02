9302cb01ddd7ac975136b2c848df7c64ed2365cc
bSlope8348 <bSlope8348@gmail.com>
2025-10-02 16:43:54 -0600

    Chapter 18 tutuorial complete

--- game/main.lua
+++ game/main.lua
@@ -1,31 +1,80 @@
-local jumps, currentFrame, image, maxJumps,	frame_width, frame_height, width, height
+local tilemap, image, width, height, quads, player
+local isEmpty
 
 function love.load()
-	image = love.graphics.newImage('assets/jumps/jump_border.png')
-	jumps = {}
-	frame_width = 117 --image:getWidth() / 3
-	frame_height = 233 --image:getHeight() / 2
-	width = image:getWidth()
-	height = image:getHeight()
-	maxJumps = 5
-	for i=0,1 do
-		for j=0,2 do
-			table.insert(jumps, love.graphics.newQuad(1 + j * (frame_width + 2), 1 + i * (frame_height + 2), frame_width, frame_height, width, height))
-			if #jumps == maxJumps then
-				break
-			end
-		end
-	end
-	currentFrame = 1
-end
+    image = love.graphics.newImage("assets/tileset.png")
 
-function love.update(dt)
-	currentFrame = currentFrame + dt * 10
-	if currentFrame >= 6 then
-		currentFrame = 1
-	end
+    local image_width = image:getWidth()
+    local image_height = image:getHeight()
+    width = (image_width / 3) - 2
+    height = (image_height / 2) - 2
+
+    quads = {}
+
+    for i=0,1 do
+        for j=0,2 do
+            table.insert(quads,
+                love.graphics.newQuad(
+                    1 + j * (width + 2),
+                    1 + i * (height + 2),
+                    width, height,
+                    image_width, image_height))
+        end
+    end
+
+	tilemap = {
+		{1, 6, 6, 2, 1, 6, 6, 2},
+		{3, 0, 0, 4, 5, 0, 0, 3},
+		{3, 0, 0, 0, 0, 0, 0, 3},
+		{4, 2, 0, 0, 0, 0, 1, 5},
+		{1, 5, 0, 0, 0, 0, 4, 2},
+		{3, 0, 0, 0, 0, 0, 0, 3},
+		{3, 0, 0, 1, 2, 0, 0, 3},
+		{4, 6, 6, 5, 4, 6, 6, 5}
+	}
+
+	--Create our player
+    player = {
+        image = love.graphics.newImage("assets/player.png"),
+        tile_x = 2,
+        tile_y = 2
+    }
 end
 
 function love.draw()
-	love.graphics.draw(image, jumps[math.floor(currentFrame)], 100, 100)
+    for i,row in ipairs(tilemap) do
+        for j,tile in ipairs(row) do
+            if tile ~= 0 then
+                --Draw the image
+                love.graphics.draw(image, quads[tile], j * width, i * height)
+            end 
+        end
+    end
+
+	--Draw the player and multiply its tile position with the tiem width and height
+	love.graphics.draw(player.image, player.tile_x * width, player.tile_y * height)
+end
+
+function love.keypressed(key)
+	local x = player.tile_x
+	local y = player.tile_y
+
+	if key == "left" then 
+		x = x - 1
+	elseif key == "right" then
+		x = x + 1
+	elseif key == "up" then
+		y = y - 1
+	elseif key == "down" then
+		y = y + 1
+	end
+
+    if isEmpty(x, y) then
+        player.tile_x = x
+        player.tile_y = y
+    end
+end
+
+function isEmpty(x, y)
+    return tilemap[y][x] == 0
 end
---
+++ game/old main/main4.lua
@@ -0,0 +1,32 @@
+--[[local jumps, currentFrame, image, maxJumps,	frame_width, frame_height, width, height
+
+function love.load()
+	image = love.graphics.newImage('assets/jumps/jump_border.png')
+	jumps = {}
+	frame_width = 117 --image:getWidth() / 3
+	frame_height = 233 --image:getHeight() / 2
+	width = image:getWidth()
+	height = image:getHeight()
+	maxJumps = 5
+	for i=0,1 do
+		for j=0,2 do
+			table.insert(jumps, love.graphics.newQuad(1 + j * (frame_width + 2), 1 + i * (frame_height + 2), frame_width, frame_height, width, height))
+			if #jumps == maxJumps then
+				break
+			end
+		end
+	end
+	currentFrame = 1
+end
+
+function love.update(dt)
+	currentFrame = currentFrame + dt * 10
+	if currentFrame >= 6 then
+		currentFrame = 1
+	end
+end
+
+function love.draw()
+	love.graphics.draw(image, jumps[math.floor(currentFrame)], 100, 100)
+end
+]]
