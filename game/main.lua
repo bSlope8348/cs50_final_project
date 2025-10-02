70095675c841dde819addae82c7801b19456af3e
bSlope8348 <bSlope8348@gmail.com>
2025-10-02 17:26:51 -0600

    Added Key and Door

--- game/main.lua
+++ game/main.lua
@@ -1,5 +1,5 @@
-local tilemap, image, width, height, quads, player
-local isEmpty
+local tilemap, image, width, height, quads, player, keyRed, has_red_key, doorRed
+local isOpenSpace, keyAquired, doorAndKey
 
 function love.load()
     image = love.graphics.newImage("assets/tileset.png")
@@ -23,14 +23,14 @@ function love.load()
     end
 
 	tilemap = {
-		{1, 6, 6, 2, 1, 6, 6, 2},
-		{3, 0, 0, 4, 5, 0, 0, 3},
-		{3, 0, 0, 0, 0, 0, 0, 3},
-		{4, 2, 0, 0, 0, 0, 1, 5},
-		{1, 5, 0, 0, 0, 0, 4, 2},
-		{3, 0, 0, 0, 0, 0, 0, 3},
-		{3, 0, 0, 1, 2, 0, 0, 3},
-		{4, 6, 6, 5, 4, 6, 6, 5}
+		{1, 6, 6, 2, 1, 6, 6, 2, 1, 6, 6, 2, 1, 6, 6, 2},
+		{3, 0, 0, 4, 5, 0, 0, 3, 3, 0, 0, 4, 5, 0, 0, 3},
+		{3, 0, 0, 0, 0, 0, 0, 3, 3, 0, 0, 0, 0, 0, 0, 3},
+		{4, 2, 0, 0, 0, 0, 1, 5, 4, 2, 0, 0, 0, 0, 1, 5},
+		{1, 5, 0, 0, 0, 0, 4, 2, 1, 5, 0, 0, 0, 0, 4, 2},
+		{3, 0, 0, 0, 0, 0, 0, 3, 3, 0, 0, 0, 0, 0, 0, 3},
+		{3, 0, 0, 1, 2, 0, 0, 3, 3, 0, 0, 1, 2, 0, 0, 3},
+		{4, 6, 6, 5, 4, 6, 6, 5, 4, 6, 6, 5, 4, 6, 6, 5}
 	}
 
 	--Create our player
@@ -39,6 +39,21 @@ function love.load()
         tile_x = 2,
         tile_y = 2
     }
+
+	--Create keys
+	has_red_key = 0
+	keyRed = {
+		image = love.graphics.newImage("assets/kenney/PNG/Power-ups/powerupRed_star.png"),
+		tile_x = 5,
+		tile_y = 6
+	}
+
+	--Create Doors
+	doorRed = {
+		image = love.graphics.newImage("assets/kenney/PNG/Power-ups/powerupRed_star.png"),
+		tile_x = 8,
+		tile_y = 3
+	}
 end
 
 function love.draw()
@@ -53,6 +68,15 @@ function love.draw()
 
 	--Draw the player and multiply its tile position with the tiem width and height
 	love.graphics.draw(player.image, player.tile_x * width, player.tile_y * height)
+
+	--Draw key
+	love.graphics.draw(keyRed.image, keyRed.tile_x * width, keyRed.tile_y * height)
+
+	love.graphics.print("Keys Aquired:", 1 * width, 9.5 * height)
+
+	--Draw door
+	love.graphics.draw(doorRed.image, doorRed.tile_x * width, doorRed.tile_y * height, 0, 0.5, 0.5, -width/2, -height/2)
+	love.graphics.draw(doorRed.image, (doorRed.tile_x + 1) * width, doorRed.tile_y * height, 0, 0.5, 0.5, -width/2, -height/2)
 end
 
 function love.keypressed(key)
@@ -69,12 +93,33 @@ function love.keypressed(key)
 		y = y + 1
 	end
 
-    if isEmpty(x, y) then
+    if isOpenSpace(x, y) then
         player.tile_x = x
         player.tile_y = y
     end
+	if keyAquired(x, y) then
+		has_red_key = 1
+		keyRed.tile_x = 1
+		keyRed.tile_y = 10
+	end
+	if doorAndKey(x, y) then
+		tilemap[doorRed.tile_y][doorRed.tile_x] = 0
+		tilemap[doorRed.tile_y][doorRed.tile_x + 1] = 0
+	end
 end
 
-function isEmpty(x, y)
+function isOpenSpace(x, y)
     return tilemap[y][x] == 0
+end
+
+function keyAquired(x, y)
+	if x == keyRed.tile_x and y == keyRed.tile_y then
+		return true
+	end
+end
+
+function doorAndKey(x, y)
+	if x == doorRed.tile_x and y == doorRed.tile_y and has_red_key == 1 then
+		return true
+	end
 end
